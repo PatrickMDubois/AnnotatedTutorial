19bf668bec8c3b708c48a9c15f944acd1949209b
Patrick Dubois <patrick.marcel.dubois@gmail.com>
2015-07-02 13:23:57 -0500

    Started adding different note categories + styles.

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -61,6 +61,7 @@ angular.module('AnnotatedTutorial')
 
                     if($scope.selectedLine > -1 && $scope.newNote) {
                         $scope.tutorial[$scope.selectedLine].notes.push($scope.newNote);
+                        $scope.newNote = "";
                         $scope.selectedLine = -1;
                     }
                 };
