c97dfd8b2c6c8de89738cb2924da02bae0432f85
Patrick Dubois <patrick.marcel.dubois@gmail.com>
2016-05-30 13:48:33 -0500

    adding note directive

---
+++ app/scripts/noteDirective.js
@@ -0,0 +1,12 @@
+angular.module('AnnotatedTutorial', [])
+    .directive('note', function() {
+        'use strict'
+
+        return {
+            restrict: 'E',
+            templateUrl: 'note.html',
+            scope: {tutorial: '='},
+            controller: function($scope) {/* ...*/},
+            compile: function(element) {{/* ...*/}}
+        };
+    });
