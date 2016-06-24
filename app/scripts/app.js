8c6066b4aa15b7a1bc2223feff2a04486bccf56b
Patrick Dubois <patrick.marcel.dubois@gmail.com>
2016-06-24 18:51:07 -0500

    preparing for deployment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,7 @@
 
     app.factory('annotatedTutorialServer', function() {
         if (typeof(DEVELOPMENT) === 'undefined') {
-            return 'http://redbird.cs.umanitoba.ca/tutorial-server/';
+            return 'http://redbud.cs.umanitoba.ca/';
             //return '//dorado.cs.umanitoba.ca:8000'; // production environment
         } else {
             return 'http://127.0.0.1:8000'; // development environment
