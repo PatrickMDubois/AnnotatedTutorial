38544903bbaffd7f992c75f4f9fd56206603346e
Patrick Dubois <patrick.marcel.dubois@gmail.com>
2017-06-14 15:06:27 -0500

    icon bar

--- app/scripts/mainController.js
+++ app/scripts/mainController.js
@@ -105,6 +105,7 @@ angular.module('AnnotatedTutorial')
                   $scope.stepFilter = null;
                   $scope.currentNote = null;
                   $scope.noteStepList.splice(0,$scope.noteStepList.length);
+                  document.getElementById("filter").value = "none";
                 };
 
                 $scope.newFilter = function(value){
