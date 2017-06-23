e1538145e842769edea2957b433b3295e646d479
Patrick Dubois <patrick.marcel.dubois@gmail.com>
2017-06-23 10:20:06 -0500

    making consistent with on side design

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -96,6 +96,66 @@ angular.module('AnnotatedTutorial')
             }
         }
     });
+function Note(data) {
+    this.step_id = data.step_id;
+    this.tutorial_id = data.tutorial_id;
+    this.category = data.category;
+    this.extra_info = data.extra_info;
+    this.content = data.content;
+    this.contributor = data.contributor;
+    this.user_submitted = data.user_submitted;
+    this.reply_to = data.reply_to;
+    this.deleted= data.deleted;
+    this.dateSubmitted=moment().format("MMM Do YY");
+}
+
+angular.module('AnnotatedTutorial')
+    .factory('TutorialService', function($http, annotatedTutorialServer, currentParticipant){
+        'use strict';
+
+        var contributor = null;
+
+        var promise = $http.get(annotatedTutorialServer + '/tutorials/contributor/' + currentParticipant)
+            .then(function(response) {
+
+                contributor = response.data;
+            });
+
+        return {
+            loaded: promise,
+            get: function() {
+                return contributor;
+            },
+            post: function(note) {
+                var note = new Note({
+                    step_id: note.step_id,
+                    tutorial_id: note.tutorial_id,
+                    category: note.category,
+                    extra_info: note.extra_info,
+                    content: note.content,
+                    contributor: note.contributor,
+                    user_submitted: true,
+                    reply_to: note.reply_to,
+                    deleted:false,
+                    rating:0,
+                });
+
+                $http.post(annotatedTutorialServer + '/tutorials/notes', note);
+            },
+            put: function(note,deleteChange, ratingChange){
+
+                if(deleteChange == true) {
+                    note.deleted = true;
+                }else if(ratingChange == true){
+                    note.rating = parseInt(note.rating)+1;
+                    console.log(ratingChange);
+
+                }
+                $http.put(annotatedTutorialServer + '/tutorials/note/update/' + note.id,note);
+
+            }
+        };
+    });
 angular.module('AnnotatedTutorial')
     .controller('mainController', function($scope, TutorialService, LoggerService){
         'use strict';
@@ -432,7 +492,7 @@ angular.module('AnnotatedTutorial')
         return {
             restrict: 'E',
             templateUrl: 'note.html',
-            scope: {note: '=',deleteIt: '=',rateIt:"=", addReply: '=', canShowNote: '=', baseline: '='},
+            scope: {note: '=',deleteIt: '=',rateIt:"=", addReply: '=', canShowNote: '=', baseline: '=',user: '=', general:'=', date: '=',currentNote:'=', notelist:'='},
             compile: function(element) {
                 return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){});
             }
