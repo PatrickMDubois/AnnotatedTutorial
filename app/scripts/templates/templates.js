ac7379125a6cc2a432f0749df8d0eab5d112d83a
Patrick Dubois <patrick.marcel.dubois@gmail.com>
2017-06-30 14:20:04 -0500

    working on reply..

--- app/scripts/templates/templates.js
+++ app/scripts/templates/templates.js
@@ -8,9 +8,10 @@ module.run(["$templateCache", function($templateCache) {
     "    <div class=\"note-contributor\">Submitted by {{note.contributor}}•</div>\n" +
     "    <div class=\"date\">{{date}}<br></div>\n" +
     "    <div class=\"note-content\">{{note.content}}</div>\n" +
-    "    <button ng-if=\"note.contributor==user.name\" ng-click=\"deleteIt(note.id)\" class=\"plain-button delete-button\">DELETE</button>\n" +
+    "\n" +
     "    <div class=\"comment-footer\">\n" +
     "        <button class=\"plain-button reply-button\" ng-click=\"addReply($index, $event, note.id, note.contributor, note.step_id)\">Reply</button>\n" +
+    "        <button ng-if=\"note.contributor==user.name\" ng-click=\"deleteIt(note.id)\" class=\"plain-button delete-button\">DELETE</button>\n" +
     "        <button class=\"rating-button\" ng-click=\"rateIt(note.id)\" tooltip=\"recommend\"></button>\n" +
     "        <div class=\"note-rating\">\n" +
     "            <div ng-if=\"note.rating>1\">{{note.rating}} people found this helpful.</div>\n" +
