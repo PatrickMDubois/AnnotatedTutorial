e1a86133e4db5b6f2b06462258758a28b12b2f1a
Patrick Dubois <patrick.marcel.dubois@gmail.com>
2017-07-17 13:53:54 -0500

    design changes

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -616,7 +616,7 @@ angular.module('AnnotatedTutorial')
         return {
             restrict: 'E',
             templateUrl: 'note.html',
-            scope: {note: '=',deleteIt: '=',rateIt:"=", addReply: '=', canShowNote: '=', user: '=', date: '=',currentNote:'=', notelist:'=', parentCategory: '='},
+            scope: {note: '=',deleteIt: '=',rateIt:"=", addReply: '=', canShowNote: '=', user: '=', date: '=',currentNote:'=', notelist:'=', replyOne: '='},
             compile: function(element) {
                 return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){});
             }
--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -5,11 +5,13 @@ module.run(["$templateCache", function($templateCache) {
   "use strict";
   $templateCache.put("note.html",
     "<div class=\"note\" ng-class=\"{\n" +
-    "     'note-corrections': note.category === 'corrections' || parentCategory === 'corrections' ,\n" +
-    "     'note-details': note.category === 'details' || parentCategory === 'details' ,\n" +
-    "     'note-questions': note.category === 'questions' || parentCategory === 'questions' ,\n" +
-    "     'note-other': note.category === 'other' || parentCategory === 'other',\n" +
+    "     'note-corrections': note.category === 'corrections',\n" +
+    "     'note-details': note.category === 'details',\n" +
+    "     'note-questions': note.category === 'questions',\n" +
+    "     'note-other': note.category === 'other',\n" +
     "     'reply-content': note.category === 'reply',\n" +
+    "     'replyOne' : replyOne,\n" +
+    "     'replyTwo' : replyOne== false,\n" +
     "     'note-replies': note.replies.length>0,\n" +
     "     'note-highlight': currentReply === note.id}\">\n" +
     "\n" +
@@ -58,9 +60,10 @@ module.run(["$templateCache", function($templateCache) {
     "            <div ng-if=\"note.contributor_list.length==1\">{{note.contributor_list.length}} person found this helpful.</div>\n" +
     "            <div ng-if=\"note.contributor_list.length<1\">Not rated yet.</div>\n" +
     "        </div>\n" +
+    "        <note ng-repeat=\"reply in note.replies\" ng-if=\"note.replies.length > 0 && canShowNote(reply)\" note=\"reply\" delete-it = \"deleteIt\" rate-it = \"rateIt\" add-reply=\"addReply\" can-show-note=\"canShowNote\" user=\"user\" date=\"date\" current-note=\"currentNote\" notelist=\"noteList\" reply-one=\"!replyOne\"></note>\n" +
+    "\n" +
     "    </div>\n" +
     "</div>\n" +
-    "<note ng-repeat=\"reply in note.replies\" ng-if=\"note.replies.length > 0 && canShowNote(reply)\" note=\"reply\" delete-it = \"deleteIt\" rate-it = \"rateIt\" add-reply=\"addReply\" can-show-note=\"canShowNote\" user=\"user\" date=\"date\" current-note=\"currentNote\" notelist=\"noteList\" parent-category=\"parentCategory\"></note>\n" +
     "");
 }]);
 })();
