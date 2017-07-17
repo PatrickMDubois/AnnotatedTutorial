7b3ebe21d1ab32eab190bd46b65c8dff71ef7bb0
Patrick Dubois <patrick.marcel.dubois@gmail.com>
2017-07-17 14:53:12 -0500

    design changes for replies

--- app/scripts/templates/templates.js
+++ app/scripts/templates/templates.js
@@ -4,7 +4,12 @@ catch(err) { module = angular.module("app-templates", []); }
 module.run(["$templateCache", function($templateCache) {
   "use strict";
   $templateCache.put("note.html",
-    "<div class=\"note\" ng-class=\"{'reply-content': note.category === 'reply', 'note-replies': note.replies.length>0 && hasReply, 'note-highlight': currentReply === note.id}\">\n" +
+    "<div class=\"note\" ng-class=\"{\n" +
+    "    'reply-content': note.category === 'reply',\n" +
+    "    'note-replies': note.replies.length>0 && hasReply,\n" +
+    "    'note-highlight': currentReply === note.id,\n" +
+    "    'replyOne': replyOne,\n" +
+    "    'replyTwo': replyOne== false}\">\n" +
     "    <div class=\"note-contributor\">{{note.contributor}}</div>\n" +
     "    <div class=\"date\">{{date}}<br></div>\n" +
     "    <div class=\"note-content\">{{note.content}}</div>\n" +
@@ -19,7 +24,7 @@ module.run(["$templateCache", function($templateCache) {
     "            <div ng-if=\"note.contributor_list.length==1\">{{note.contributor_list.length}} person found this helpful.</div>\n" +
     "            <div ng-if=\"note.contributor_list.length<1\">Not rated yet.</div>\n" +
     "        </div>\n" +
-    "        <note ng-repeat=\"reply in note.replies\" ng-if=\"note.replies.length > 0 && canShowNote(reply)\" note=\"reply\" delete-it = \"deleteIt\" rate-it = \"rateIt\" add-reply=\"addReply\" can-show-note=\"canShowNote\" user=\"user\" date=\"date\" current-reply=\"currentReply\" has-reply=\"hasReply\"></note>\n" +
+    "        <note ng-repeat=\"reply in note.replies\" ng-if=\"note.replies.length > 0 && canShowNote(reply)\" note=\"reply\" delete-it = \"deleteIt\" rate-it = \"rateIt\" add-reply=\"addReply\" can-show-note=\"canShowNote\" user=\"user\" date=\"date\" current-reply=\"currentReply\" has-reply=\"hasReply\" reply-one=\"!replyOne\"></note>\n" +
     "    </div>\n" +
     "</div>\n" +
     "");
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -521,7 +521,7 @@ angular.module('AnnotatedTutorial')
         return {
             restrict: 'E',
             templateUrl: 'note.html',
-            scope: {note: '=',deleteIt: '=',rateIt:"=", addReply: '=', canShowNote: '=', user: '=', date: '=', currentReply: '=', hasReply:'='},
+            scope: {note: '=',deleteIt: '=',rateIt:"=", addReply: '=', canShowNote: '=', user: '=', date: '=', currentReply: '=', hasReply:'=', replyOne: '='},
             compile: function(element) {
                 return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){});
             }
