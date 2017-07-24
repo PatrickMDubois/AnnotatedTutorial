74938a9594a43f17124ae62b066f17b9e8546987
Patrick Dubois <patrick.marcel.dubois@gmail.com>
2017-07-24 15:30:18 -0500

    design changes

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -20,23 +20,19 @@ module.run(["$templateCache", function($templateCache) {
     "        <div ng-if=\"notelist.length<1\"> No Associated Step</div>\n" +
     "    </div>\n" +
     "    <div ng-if=\"!baseline && note.category=='corrections'\">\n" +
-    "        <div class=\"filter-icon filter-icon-corrections note-icon\" style=\"margin-top:5px; margin-right:5px;\"></div>\n" +
+    "        <div class=\"filter-icon-corrections note-icon\" style=\"margin-right:5px; background-color: #ffc497;\"></div>\n" +
     "        <div class=\"note-contributor\">Corrections</div>\n" +
     "    </div>\n" +
-    "    <div ng-if=\"!baseline && note.category=='methods'\">\n" +
-    "        <div class=\"filter-icon filter-icon-methods note-icon\" style=\"margin-top:5px; margin-right:5px;\"></div>\n" +
-    "        <div class=\"note-contributor\">What is this?</div>\n" +
-    "    </div>\n" +
     "    <div ng-if=\"!baseline  && note.category=='details'\">\n" +
-    "        <div class=\"filter-icon filter-icon-details note-icon\" style=\"margin-top:5px; margin-right:5px;\"></div>\n" +
+    "        <div class=\"filter-icon-details note-icon\" style=\"margin-right:5px; background-color:#e0d4ff;\"></div>\n" +
     "        <div class=\"note-contributor\">Tell Me More!</div>\n" +
     "    </div>\n" +
     "    <div ng-if=\"!baseline && note.category=='questions'\">\n" +
-    "        <div class=\"filter-icon filter-icon-questions note-icon\" style=\"margin-top:5px; margin-right:5px;\"></div>\n" +
+    "        <div class=\"filter-icon-questions note-icon\" style=\" margin-right:5px; background-color:#fff07d;\"></div>\n" +
     "        <div class=\"note-contributor\">Questions</div>\n" +
     "    </div>\n" +
-    "    <div ng-if=\"!baseline && note.category=='other'\" style=\"margin-top:5px\">\n" +
-    "        <div class=\"filter-icon filter-icon-other note-icon\" style=\"margin-top:5px; margin-right:5px;\"></div>\n" +
+    "    <div ng-if=\"!baseline && note.category=='other'\">\n" +
+    "        <div class=\"filter-icon-other note-icon\" style=\" margin-right:5px; background-color: #d4ed96;\"></div>\n" +
     "        <div class=\"note-contributor\">Other</div>\n" +
     "    </div>\n" +
     "\n" +
